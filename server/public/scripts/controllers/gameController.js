eefb25c95829f173759e4ca804b141a50cf1ad2b
apeloza <apeloza@alumni.uwo.ca>
2016-06-06 22:33:18 -0500

    Here comes dat boi

--- server/public/scripts/controllers/gameController.js
+++ server/public/scripts/controllers/gameController.js
@@ -1,10 +1,22 @@
 app.controller('GameController', ['$scope', '$http', 'DataFactory', function($scope, $http, DataFactory) {
 
+$scope.advanceText = function() {
+var currIndex = $scope.lines.indexOf($scope.line);
+var nextIndex = currIndex + 1;
+$scope.line = $scope.lines[nextIndex];
+if($scope.line.character){
+  $scope.currChar = DataFactory.getCharacter($scope.line.character);
+}
+if($scope.line.emotion){
+  $scope.emotion = $scope.line.emotion;
+}
+};
     DataFactory.initialize().then(function(){
-      $scope.currChar = DataFactory.getCharacter('witnessOne');
+      $scope.currChar = DataFactory.getCharacter('detective');
       $scope.currScene = DataFactory.getScene('opening');
-      $scope.emotion = "sweating";
-
+      $scope.emotion = "default";
+      $scope.lines = $scope.currScene.lines;
+      $scope.line = $scope.lines[0];
     });
 
 }]);
