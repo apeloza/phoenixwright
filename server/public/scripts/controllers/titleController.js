6e2d80a6a15565df86d17a1d26583bdf0f4c5332
apeloza <apeloza@alumni.uwo.ca>
2016-06-17 14:01:42 -0500

    Added comments and cleaned up code

--- server/public/scripts/controllers/titleController.js
+++ server/public/scripts/controllers/titleController.js
@@ -1,14 +1,22 @@
 app.controller('TitleController', ['$scope', '$http', '$timeout', 'ngAudio', 'DataFactory', function($scope, $http, $timeout, ngAudio, DataFactory) {
+
+//Prepares easter egg music
 var eggmusic = ngAudio.load('../assets/audio/bgm/egg.mp3');
+
+//Sets background with ng-style
 $scope.background = {
   'background-position': '-2217px -580px'
 };
+
+//Sets case# and then the anchor directs us to case.html/gamecontroller
   $scope.clickCase = function(caseNum){
     eggmusic.stop();
     DataFactory.setCaseNum(caseNum);
     var selectsound = ngAudio.load('../assets/audio/sfx/sfx-selectblip.wav');
     selectsound.play();
   };
+
+  //Easter egg activates
 $scope.datboi = function(){
   if ($scope.egg === true){
     $scope.egg = false;
--- server/public/scripts/factories/dataFactory.js
+++ server/public/scripts/factories/dataFactory.js
@@ -7,58 +7,25 @@ app.factory('DataFactory', ['$http', '$q', function($http, $q) {
     var evidence;
     var casenum;
 
-function setCaseNum(clickednum){
-casenum = clickednum;
-}
-function getCaseNum(){
-  return casenum;
-}
-
+    //Saves all characters of a specific case in memory
     function getChars() {
-        var promise = $http.get('/case' + casenum+ '/characters').then(function(response) {
+        var promise = $http.get('/case' + casenum + '/characters').then(function(response) {
             console.log('Async data returned: ', response.data);
             characters = response.data;
         });
         return promise;
     }
 
+    //Saves all scenes of a specific case in memory
     function getScenes() {
-        var promise = $http.get('/case' + casenum +'/scenes').then(function(response) {
+        var promise = $http.get('/case' + casenum + '/scenes').then(function(response) {
             console.log('Async data returned: ', response.data);
             scenes = response.data;
         });
         return promise;
     }
 
-    function getChar(name) {
-        return characters[name];
-    }
-
-    function getScene(name) {
-        return scenes[name];
-    }
-
-    function scenesArray() {
-        return scenes;
-    }
-function sceneOrder(){
-  return scenes.sceneArray;
-}
-    function evidenceList() {
-        return evidence;
-    }
-
-    function getEvidenceItem(name) {
-        return evidence[name];
-    }
-
-    function initialize() {
-        var promises = [getChars(), getScenes(), getEvidence()];
-        return $q.all(promises);
-    }
-
-
-
+    //Saves all evidence of a given case in memory
     function getEvidence() {
         var promise = $http.get('/case' + casenum + '/evidence').then(function(response) {
             console.log('Async data returned: ', response.data);
@@ -66,7 +33,49 @@ function sceneOrder(){
         });
         return promise;
     }
+    //Saves the clicked case so that both controllers can access it.
+    function setCaseNum(clickednum) {
+        casenum = clickednum;
+    }
 
+    //Returns the current case #.
+    function getCaseNum() {
+        return casenum;
+    }
+
+
+
+    //Returns a specific character
+    function getChar(name) {
+        return characters[name];
+    }
+
+    //Returns a specific scene
+    function getScene(name) {
+        return scenes[name];
+    }
+
+    //Returns an array of all scenes
+    function scenesArray() {
+        return scenes;
+    }
+
+    //Returns the array that specifies what order the scenes must be loaded in. This is necessary because by default angular sorts the scenes alphabetically when fetching them.
+    function sceneOrder() {
+        return scenes.sceneArray;
+    }
+
+    //Returns all evidence for the particular case.
+    function evidenceList() {
+        return evidence;
+    }
+
+    //Returns a specific evidence item
+    function getEvidenceItem(name) {
+        return evidence[name];
+    }
+
+    //Returns how many evidence items are in a given case
     function getEvidenceLength() {
         var length = 0;
         for (var key in evidence) {
@@ -76,6 +85,17 @@ function sceneOrder(){
         }
         return length;
     }
+
+    //Required to start the game -- this makes sure that all of our JSON files are loaded before proceeding
+    function initialize() {
+        var promises = [getChars(), getScenes(), getEvidence()];
+        return $q.all(promises);
+    }
+
+
+
+
+
 
 
     //PUBLIC
